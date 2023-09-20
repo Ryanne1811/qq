6f71cf2d05db332dbeee4ee0153e59eb0a42031e
teng laurie <laurie051118@gmail.com>
2023-09-20 16:07:49 +0800

    ok

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, request
+import random
+import string
+import os
 
-# 載入 json 標準函式庫，處理回傳的資料格式
-import json
-
-# 載入 LINE Message API 相關函式庫
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageMessage
 
 app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi('c2evFAGICqUJHssSolgeTP1c+FJOC/up+zxqSBnuTMgcxiC6LS5NGqivDdS+T+jv85045Z2udt+OD9H/Y1CG8TpR3s257zNIO7kZofSvMCeyt/sRe8BBhg2FlQTmSKrQxUeIlVEUI+PLrh/Ctt8qqAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('bfb7c602056292ca553e3455b3b91a07')
 
-@app.route("/", methods=['POST'])
-def linebot():
-    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 处理 LINE 的 Webhook 请求
+    # ...
+    body = request.get_data(as_text=True)
+    signature = request.headers['X-Line-Signature']
+    
     try:
-        json_data = json.loads(body)                         # json 格式化訊息內容
-        access_token = 'c2evFAGICqUJHssSolgeTP1c+FJOC/up+zxqSBnuTMgcxiC6LS5NGqivDdS+T+jv85045Z2udt+OD9H/Y1CG8TpR3s257zNIO7kZofSvMCeyt/sRe8BBhg2FlQTmSKrQxUeIlVEUI+PLrh/Ctt8qqAdB04t89/1O/w1cDnyilFU='
-        secret = 'bfb7c602056292ca553e3455b3b91a07'
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
-        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
-        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
-        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
-        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
-        if type=='text':
-            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
-            print(msg)                                       # 印出內容
-            reply = msg
-        else:
-            reply = '你傳的不是文字呦～'
-        print(reply)
-        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
-    except:
-        print(body)                                          # 如果發生錯誤，印出收到的內容
-    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return 'Invalid signature', 400
+    
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def callback(event):
+    message_text = event.message.text
+    reply_text = "你發送的消息是：" + message_text
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
+@handler.add(MessageEvent, message=[ImageMessage])
+def callback(event):
+    image_name = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10)) + '.jpg'
+    image_content = line_bot_api.get_message_content(event.message.id)
+    path = os.path.join(static_tmp_path, image_name)
+
+    with open(path, 'wb') as fd:
+        for chunk in image_content.iter_content():
+            fd.write(chunk)
+
+    reply_text = '圖片已保存為 ' + image_name
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    app.run()
-
-    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
